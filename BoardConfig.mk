daef23f58c5ea2d84c5de640686cff81f1250c13
TheScarastic <warabhishek@gmail.com>
2018-10-10 04:42:39 -0400

    X01AD: Enable FDE encryption
    Change-Id: I248a4b74332d823fa1e4f51df0ff3f671f86bb9c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,9 @@ USE_XML_AUDIO_POLICY_CONF := 1
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78af000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7
