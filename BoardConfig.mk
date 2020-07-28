6a809891a3e0d397bb4e19f23f6575a7158b71be
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-07-28 12:33:55 +0530

    X01AD: Import WiFi flags and build wifi packages
    Change-Id: I290e88a4b97955715db7ca6d955e4baa187720ae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,5 +165,17 @@ BOARD_VNDK_VERSION := current
 # Vendor
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Wi-Fi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+
 # Inherit from the proprietary version
 -include vendor/asus/X01AD/BoardConfigVendor.mk
