1bd449e1178b6cdf5d533faeb1cbd59dd5976d70
iamsaalim <saalimquadri1@gmail.com>
2020-10-25 01:18:39 +0530

    X01AD: Initial device makefiles
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>
    Change-Id: Ic1d339916d8ffac85c95ebdc5a48144ce13fa8d5

---
+++ device.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Asserts
+TARGET_OTA_ASSERT_DEVICE := X01A,X01AD
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1520
+TARGET_SCREEN_WIDTH := 720
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/asus/X01AD/X01AD-vendor.mk)
