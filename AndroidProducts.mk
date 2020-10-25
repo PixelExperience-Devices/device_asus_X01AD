9668fb7e89835433e28a53de28ea89ec3509700d
iamsaalim <saalimquadri1@gmail.com>
2020-10-25 09:54:00 +0530

    X01AD: Call product via aosp way
    Change-Id: I69f53cb79fd5fadf8a9cec4a79dfe1c63ff364db

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_X01AD.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_X01AD-user \
+    lineage_X01AD-userdebug \
+    lineage_X01AD-eng
---
+++ lineage_X01AD.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/asus/X01AD
+
+# Inherit from source
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Set shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device stuff
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Device identifiers.
+PRODUCT_DEVICE := X01AD
+PRODUCT_NAME := lineage_X01AD
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_X01AD
+PRODUCT_MANUFACTURER := asus
+TARGET_VENDOR := asus
+BOARD_VENDOR := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+BUILD_FINGERPRINT := "asus/WW_X01AD/ASUS_X01A_1:10/WW_Phone-202005071625/17.2018.2004.31-20200507:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="msm8953_64-user 10 WW_Phone-202005071625 17.2018.2004.31-20200507 release-keys"
