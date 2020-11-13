6cde90d05f971a15d56c88f537c66ecca44e7a55
iamsaalim <saalimquadri1@gmail.com>
2020-11-13 12:28:12 +0530

    X01AD: Import ramdisk from LA.UM.8.6.2.r1-08400-89xx.0
    * Cleaned up unused CAF scripts
    * Build them too
    
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>
    Change-Id: I2303d69a0252f8458dd93ac8f4ab34dc3cf77bf3

--- device.mk
+++ device.mk
@@ -103,9 +103,22 @@ PRODUCT_PACKAGES += \
 
 # Init
 PRODUCT_PACKAGES += \
-    init.safailnet.rc \
+    fstab.qcom \
+    init.msm.usb.configfs.rc \
     init.qcom.rc \
-    fstab.qcom
+    init.qcom.usb.rc \
+    init.safailnet.rc \
+    init.target.rc \
+    ueventd.qcom.rc
+
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.qcom.class_core.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh
 
 # Keylayout
 PRODUCT_COPY_FILES += \
