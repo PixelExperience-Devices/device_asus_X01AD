6ee68ca085d9543fe3d85085c02f95104ad9a3f7
danascape <saalimquadri1@gmail.com>
2020-12-10 06:07:37 +0000

    Revert "X01AD: Build system variant of livedisplay-2.0"
    This reverts commit ecd37d872e0428a62ea5423c9636e025c0120daf.
    
    Change-Id: I73eb20dbd0fb446c9e12310394209fa5c1534756

--- device.mk
+++ device.mk
@@ -249,10 +249,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.asus_X01AD
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml::$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
