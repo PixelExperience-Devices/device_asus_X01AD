95097024f79eccdb91b36ce74a6a3d3405ea64ae
Brothereye <99174654+BrotherI01@users.noreply.github.com>
2022-02-10 13:37:53 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X01AD.mk
+    $(LOCAL_DIR)/rr_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X01AD-user \
-    aosp_X01AD-userdebug \
-    aosp_X01AD-eng
+    rr_X01AD-user \
+    rr_X01AD-userdebug \
+    rr_X01AD-eng
